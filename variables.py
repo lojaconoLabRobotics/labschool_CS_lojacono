5bd08e1a534b19c1c2d97dcf7d659936540a976e
lojaconoLabRobotics <samuel.lojacono@labschool.org>
2024-09-11 18:10:26 +0000

    Upload variables.py file

---
+++ variables.py
@@ -0,0 +1,18 @@
+print("Good morning class!")
+
+# this is a comment.  It is used for leaving comments in your code.
+# Comments are ignored by the computer, so they are not run.
+# Comments are used to leave notes for other people reading your code.
+
+x = 17
+y = 25
+print(x+y)
+
+print(x*y)  #I am multiplying x and y
+print(x/y) #I am dividing x and y
+print(x-y) #I am subtracting x and y
+print(x^y) #I am raising x to the power of y
+
+thisIsAVariable = "Hello everyone! Welcome to Python."
+print(thisIsAVariable)
+
