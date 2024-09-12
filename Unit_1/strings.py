c29a27d1729a5184633804521c66224c3c56aac5
lojaconoLabRobotics <samuel.lojacono@labschool.org>
2024-09-12 13:56:09 +0000

    Created new files

---
+++ Unit_1/strings.py
@@ -0,0 +1,23 @@
+
+myString = "Hello world"
+
+#len() returns the length of a string
+
+print(len(myString))
+
+pledge = "I pledge allegiance to the flag of the United States of America."
+
+#how many characters are here? Use the len() function, and then print it out.
+print(len(pledge))
+
+landOfConfusion = "I must've dreamed a thousand dreams, haunted by a thousand screams.  But I can hear the marching of feet, they're moving into the street."
+
+print("")
+
+rhythmOfMyHeart = "The rhythm of my heart is beating like a drum.  The words I love you rolling off my tongue."
+
+dontDreamItsOver = "There's freedom within; there's freedom without."
+
+print(len(landOfConfusion))
+print(len(rhythmOfMyHeart))
+print(len(dontDreamItsOver))
