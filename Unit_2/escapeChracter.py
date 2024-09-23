2093aaef89533748ddbc46883966935d29d5a5c9
lojaconoLabRobotics <samuel.lojacono@labschool.org>
2024-09-23 09:57:23 -0400

    Add files via upload

---
+++ Unit_2/escapeChracter.py
@@ -0,0 +1,18 @@
+
+#Example 1
+palpatineAnnouncement = "In order to ensure the security and continuing stability,"
+palpatineAnnouncement2 = "the Republic will be RE-organized, into the first GALACTIC EMPIRE!"
+palpatineAnnouncement3 = "For a safe and SECURE... SOCIETY!"
+
+#print this out as three lines using f-strings and escape character
+palpatine = f"{palpatineAnnouncement}\n{palpatineAnnouncement2}\n{palpatineAnnouncement3}"
+print(palpatine)
+
+print()
+
+#Example 2
+andrews = "That's five compartments!"
+andrews2 = "She can stay afloat with the first 4 compartments breached, but not five.  NOT FIVE."
+#print this out as two lines using f-strings and escape character
+fullAndrews = f"{andrews}\n{andrews2}"
+print(fullAndrews)
